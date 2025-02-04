929d01d336704d02a6d53240a25f78feb3fda419
VB-Python <barbosa.vitor@gmail.com>
2025-02-04 20:54:39 +0000

    test

--- ex04.py
+++ ex04.py
@@ -1,3 +1,16 @@
-a=3
-b=5
-print (2*a * 3*b)
+''' Escreve um programa que calcule o volume de uma esfera. O valor do raio deverá ser 
+introduzido pelo utilizador (deverá ser criado o ficheiro ex04.py). '''
+
+import math
+
+pi = math.pi
+
+raio = input ("Introduza um valor para o raio:\n---->\t")
+raio = float(raio)
+
+volume = round (4/3*pi*raio**3, 2)
+
+output = f"o volume da esfera com raio {raio} é {volume}" #formated stringd
+
+print (output)
+              
