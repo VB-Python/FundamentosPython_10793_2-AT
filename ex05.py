1c71673a4a4468d4d4cdec208e9cf08d4dc3001c
VB-Python <barbosa.vitor@gmail.com>
2025-02-04 21:18:23 +0000

    test

--- ex05.py
+++ ex05.py
@@ -1,3 +1,7 @@
+'''Sejam a e b os catetos de um triângulo retângulo, faz um programa que devolva o valor
+da hipotenusa (deverá ser criado o ficheiro ex05.py).'''
+
+
 
 print("Programa para calcular o valor da hipotenusa de um triangulo-rectangulo")
 
---
+++ ex06.py
@@ -0,0 +1,4 @@
+''' Faz um programa que receba três parâmetros inteiros (horas, minutos e segundos) e
+converta o resultado para segundos, devolvendo o output para o ecrã (deverá ser
+criado o ficheiro ex06.py).'''
+
