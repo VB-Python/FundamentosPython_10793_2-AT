d8806813c68b0c2c1a97d3822764213b4643401b
VB-Python <barbosa.vitor@gmail.com>
2025-02-05 00:56:43 +0000

    test

--- FT03/ex10.py
+++ FT03/ex10.py
@@ -4,22 +4,21 @@ Caso, o utilizador escolha divisão e insira como valor do denominar 0 mostra um
 mensagem personalizada. Para os restantes casos, mostra no ecrã o resultado da
 operação desejada.'''
 
-operacao    = bool(input("especifique a operação desejada (+,-,*,/)"))
-numero1     = int(input("Insira um primeiro valor"))
-numero2     = int(input("Insira um segundo valor"))
+operacao    = str(input("especifique a operação desejada (+,-,*,/):"))
+numero1     = int(input("Insira um primeiro valor:"))
+numero2     = int(input("Insira um segundo valor:"))
 conta       = float
 
 
 if operacao == '+':
-    print(numero1 + numero2)
+    print("Resultado da operação é:" ,numero1 + numero2,)
 elif operacao == '-':
-    print(numero1 - numero2)
+    print("Resultado da operação é:" ,numero1 - numero2,)
 elif operacao == '*':
-    print(numero1 * numero2)
+    print("Resultado da operação é:" ,numero1 * numero2,)
 elif operacao == '/':
     if numero2 == 0:
            print("Não é possivel dividir por zero")
     else:
-    #print(numero1 / numero2)
-
-    
+        print("Resultado da operação = " ,numero1 / numero2,)
+        
