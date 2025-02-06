ab02ea123edbc5f19561364d502e6f23b8e1bd03
VB-Python <barbosa.vitor@gmail.com>
2025-02-06 22:01:55 +0000

    teste

--- FT03/ex10.py
+++ FT03/ex10.py
@@ -21,4 +21,28 @@ elif operacao == '/':
            print("Não é possivel dividir por zero")
     else:
         print("Resultado da operação = " ,numero1 / numero2,)
-        
+else:
+    print("Operação inválida")
+
+
+
+
+
+ #Impletemente uma calculadora simples com as operações aritméticas básicas. O utilizador deverá especificar a operação desejada (+,-,*,/) e, em seguida, inserir dois valores.
+#Caso o utilizador escolha divisão e insira como valor do denominador 0 mostra uma mensagem personalizada. Para os restantes casos, mostra no ecrã o resultado da operação desejada.
+operação = input("Operação:")
+n1 = float(input("N1:"))
+n2 = float(input("N2:"))
+if operação == "+":
+    print(n1+n2)
+elif operação == "-":
+    print(n1-n2)
+elif operação == "*":
+    print(n1*n2)
+elif operação == "/":
+    if n2 == 0:
+        print("Não pode dividir um número por 0")
+    else:
+        print(n1/n2)
+else:
+    print("Operação inválida")
