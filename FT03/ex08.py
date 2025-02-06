25538307d0af9747b610d5a434e812e7b3198252
VB-Python <barbosa.vitor@gmail.com>
2025-02-06 21:23:31 +0000

    teste

--- FT03/ex08.py
+++ FT03/ex08.py
@@ -17,3 +17,14 @@ elif lado1 == lado2 != lado3 or lado1 != lado2 == lado3 or lado1 == lado3 != lad
     print("Triângulo isósceles: caracterizado por ter dois lados com o mesmo comprimento")
 else:
     print("Triângulo escaleno: todos os lados possuem comprimento diferente")
+
+
+lado_a = float(input("Digite o primeiro lado\n "))
+lado_b = float(input("Digite o segundo lado\n "))
+lado_c = float(input("Digite o terceiro lado\n "))
+if lado_a == lado_b == lado_c:
+    print("Triângulo equilátero")
+elif lado_a != lado_b != lado_c:
+    print("Triângulo escaleno")
+else:
+    print("Triângulo isósceles")
