d8c906b46b9e03f8ea80f0a7d4819c8e56602f8a
VB-Python <barbosa.vitor@gmail.com>
2025-02-06 21:31:45 +0000

    teste

--- FT03/ex09.py
+++ FT03/ex09.py
@@ -38,3 +38,31 @@ elif imc >= 40:
     print(nome,"," , idade, "anos, ",peso, "Kg, e" ,altura, "metros de altura :" , imc, "IMC>=40 - Obesidade III (mórbida)")
 
 
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+IMC = round(peso / (altura * altura), 2)
+if IMC < 17:
+    
+    print(f"{nome} seu IMC é {IMC} você está muito abaixo do peso ideal!")
+elif IMC >= 17 and IMC < 18.5:
+    print(f"{nome} seu IMC é {IMC} você está abaixo do peso!")
+    
+elif IMC >= 18.5 and IMC < 25:
+    print(f"{nome} seu IMC é {IMC} você está no peso normal!")
+    
+elif IMC >= 25 and IMC < 30:
+    
+    print(f"{nome} seu IMC é {IMC} você está acima do peso!")
+    
+elif IMC >= 30 and IMC < 35:
+    
+    print(f"{nome} seu IMC é {IMC} você está em obesidade I!")
+elif IMC >= 35 and IMC < 40:
+    
+    print(f"{nome} seu IMC é {IMC} você está em obesidade II (severa)!")
+    
+else:
+    
+    print(f"{nome} seu IMC é {IMC} você está em obesidade III (mórbida)!")
