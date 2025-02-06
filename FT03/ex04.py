3e3b3668355a6f9d150af851c70a065b0287f71b
VB-Python <barbosa.vitor@gmail.com>
2025-02-06 20:20:33 +0000

    teste

--- FT03/ex04.py
+++ FT03/ex04.py
@@ -13,3 +13,11 @@ else:
     print("Indicou 2 numeros iguais:" ,nreal1,)
 
 
+x = float(input("Digite um número inteiro x:"))
+y = float(input("Digite um número inteiro y:"))
+if x > y:
+    print(x, "é maior do que", y)
+elif x == y:
+    print(x, "é igual a", y) 
+else:
+    print(x, "é menor do que", y)
