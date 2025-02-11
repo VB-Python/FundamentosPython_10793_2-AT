57fb571a1479b74e728aa1035ad22b6bb755bfa3
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 22:41:25 +0000

    vcbbdf

--- FT04/ex11.py
+++ FT04/ex11.py
@@ -1,3 +1,13 @@
 ''' Escreve um programa que calcule a soma e o produto dos N primeiros números naturais.
 '''
 
+n = int(input("Introduza um numero:"))
+N = n+1
+soma = 0
+multiplicacao = 1
+for i in range(1,N):
+    soma = soma + i
+    multiplicacao = multiplicacao * i
+
+print(f"soma: {soma}")
+print(f"multiplicacao: {multiplicacao}")
