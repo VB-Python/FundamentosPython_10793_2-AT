a216c2e76c159864e9495dea1091d67969666e6e
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:24:49 +0000

    ghnhgn

--- FT04/ex03.py
+++ FT04/ex03.py
@@ -1,14 +1,47 @@
 '''Fazer um programa para ler quatro números inteiros e positivos, calcular e devolver a sua
 média.'''
 
-contador = 1
+contador = 0
 soma = 0
 
 while contador < 4:
-    numero = int(input(f"Introduza 4 numerospara efectuar a media"))
+    numero = int(input(f"Introduza 4 numerospara efectuar a media :"))
     soma = soma + numero
     contador = contador + 1
 
 media = soma / contador
-
+print(contador)
 print(f"A media é {media}")
+
+
+
+
+'''Fazer um programa para ler quatro números inteiros e positivos, calcular e devolver  a  sua 
+média. - usando o ciclo while'''
+contador = 0
+soma = 0
+while contador <4:
+    numero = int(input(f"Introduza o {contador+1}º número: "))
+    soma = soma + numero     
+    contador =contador + 1
+    
+print(contador)
+media = soma / contador
+print(f"A média dos números introduzidos é {media}")
+    
+# x+=1 é o mesmo que x = x + 1
+# x-=1 é o mesmo que x = x - 1
+# x*=1 é o mesmo que x = x * 1
+# x/=1 é o mesmo que x = x / 1
+# x%=1 é o mesmo que x = x % 1
+
+#opção 2
+contador = 1
+soma = 0
+while contador <=44:
+    numero = int(input(f"Introduza o {contador}º número: "))
+    soma = soma + numero     
+    contador =contador + 1
+print(contador)
+media = soma / (contador-1)
+print(f"A média dos números introduzidos é {media}")
