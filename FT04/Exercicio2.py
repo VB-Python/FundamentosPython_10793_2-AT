afdd636f4abe690ac52e0f706c6053b2f310ffc0
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 22:16:37 +0000

    vnghn

--- FT04/Exercicio2.py
+++ FT04/Exercicio2.py
@@ -5,3 +5,12 @@ for i in range(11):
 
 print(soma)
 
+'''Escreve um programa que calcule a soma dos 10
+primeiros números inteiros positivos e devolve o
+resultado para o ecã'''
+soma=0
+for i in range(1,11):
+   
+    soma+=i
+   
+print(soma)
---
+++ FT04/exercicio3.py
@@ -0,0 +1,7 @@
+'''programa que devolve todos os numeros entre 100 e 200(exclusive), divisiveis por 3'''
+
+
+for i in range(100,200): # apresenta valores de 100 a 199
+    if i % 3 == 0: #imprime-os se divisivel por 3
+        print(i)
+
