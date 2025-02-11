b49ecff39f8b0f46ac7197e0f34762d4b8503df0
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:53:43 +0000

    mjhmhm

--- FT04/ex07.py
+++ FT04/ex07.py
@@ -3,10 +3,16 @@ em que N é um número introduzido pelo utilizador (NOTA: este programa poderia 
 utilizando a fórmula da progressão aritmética, S = (1+N) * N/2, mas faz de conta que não
 sabíamos a fórmula).'''
 
-
+natural = int(input("Introduza o numero de numeros naturais que pretende somar:"))
 numero = 1
+soma = 0
+while numero <= natural:
+    soma = soma + numero
+    print(numero)
+    numero = numero + 1 
+    
+    
 
-while numero < 100:
-    print(numero)
-    numero = numero + 1     
-    print(numero)
+print (f"quantidade de numeros naturais: {natural}, tem como resultado {soma}")
+
+    
