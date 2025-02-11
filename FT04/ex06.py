0019c7e64c4edaf01677994609218183127c19c8
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:54:47 +0000

    fdbfdb

--- FT04/ex06.py
+++ FT04/ex06.py
@@ -9,3 +9,11 @@ while numero < 100:
     numero = numero + 1     
     print(f"O quadrado de {numero} é {numero * numero}")
 
+
+
+'''Elabora um programa para escrever no ecrã os números de 1 a 100 e os respetivos
+quadrados'''
+i=1
+while i<=100:
+    print(f"{i}\t{i**2}")
+    i=i+1
