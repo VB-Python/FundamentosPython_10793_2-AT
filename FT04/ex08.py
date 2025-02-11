88069e1d80d224a51b93da1e5aa3d3734c3685a3
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 21:11:54 +0000

    nm,,

---
+++ FT04/ex08.py
@@ -0,0 +1,14 @@
+'''Escreve um programa que coloque no ecrã a tabuada do número 5.'''
+
+
+print("tabuada do número 5\n")
+numero1 = 0
+tabuada = 5
+multiplicacao = 5
+while numero1 <= 10:
+    
+    multiplicacao = tabuada * numero1 
+    print(f"{tabuada} x {numero1} = {multiplicacao}")
+    numero1 = numero1 + 1
+
+    
