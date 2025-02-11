02fd3e454481cf507fe4706e2e4b69e826c98c52
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:33:49 +0000

    gfgfb

---
+++ FT04/ex05.py
@@ -0,0 +1,11 @@
+'''Elabora um programa para escrever no ecrã os números de 1 a 100.
+'''
+
+
+
+numero = 1
+
+while numero < 100:
+    print(numero)
+    numero = numero + 1     
+    print(numero)
