e6da8e749ce6dcacb23511193ebd47ed53440909
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:37:36 +0000

    fgnng

---
+++ FT04/ex06.py
@@ -0,0 +1,11 @@
+'''Elabora um programa para escrever no ecrã os números de 1 a 100 e os respetivos
+quadrados.
+'''
+
+numero = 1
+
+while numero < 100:
+    print(numero)
+    numero = numero + 1     
+    print(f"O quadrado de {numero} é {numero * numero}")
+
