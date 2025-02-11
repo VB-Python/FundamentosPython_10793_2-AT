8b74d99d4b44aec0c7aa497a9237d1e11917f408
VB-Python <barbosa.vitor@gmail.com>
2025-02-11 20:30:23 +0000

    n,kj,k

---
+++ FT04/ex04.py
@@ -0,0 +1,11 @@
+'''Escreve um programa para escrever no ecrã a palavra olá 100 vezes.'''
+
+
+ola = str('Olá')
+palavra = 0
+while palavra <100:
+    print(ola)
+    palavra = palavra + 1     
+    
+
+print(f"Palavra {ola} escrita por {palavra}")
