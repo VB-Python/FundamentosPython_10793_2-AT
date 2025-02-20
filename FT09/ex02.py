9d48d41e970bf5b3b0aea90ba6abc0f37cbccbe2
VB-Python <barbosa.vitor@gmail.com>
2025-02-20 21:21:37 +0000

    test

---
+++ FT09/ex02.py
@@ -0,0 +1,8 @@
+'''Efetua um programa em python onde:
+a. Cries um dicionário e efetues o respetivo print.
+b. Acrescentes dois novos elementos ao dicionário.
+c. Removes um dos elementos da lista;
+d. Efetues uma operação, à escolha, sobre os dados no dicionário'''
+
+
+
