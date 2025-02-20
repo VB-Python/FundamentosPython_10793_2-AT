0a00b33b52324065f6aeae441ffa0b65a6a0f676
VB-Python <barbosa.vitor@gmail.com>
2025-02-20 22:06:28 +0000

    test

--- FT09/ex02.py
+++ FT09/ex02.py
@@ -9,11 +9,12 @@ print("a - Cria um dicionário e efetua o respetivo print")
 print("b - Acrescentes dois novos elementos ao dicionário")
 print("c - Removes um dos elementos da lista")
 print("d - Efetues uma operação, à escolha, sobre os dados no dicionário")
+
+disciplinas = {"matematica":15, "ingles":16, "historia":16}
 while True:
     user = input("Escolha uma opção:")
     match user:
         case "a":
-            disciplinas = {"matematica":15, "ingles":16, "historia":16}
             print(disciplinas)
         case "b":
             disciplinas.update({"portugues":15})
