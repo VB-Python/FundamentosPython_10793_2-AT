f1fa8b3dc89c6d117434b12910a91f944ad31bfd
VB-Python <barbosa.vitor@gmail.com>
2025-02-27 21:12:29 +0000

    test

--- FICHA_INTERMEDIA/ex05.py
+++ FICHA_INTERMEDIA/ex05.py
@@ -1,18 +1,20 @@
 '''Elabora uma script em python que peça ao utilizador um número e some todos 
 os números de 1 até esse mesmo número. Deves utilizar o tratamento de 
 erros.'''
-
-import sys
 soma = 0
+import sys
+
 try: 
     numero = int(input("Digite um número: "))
-    for j in range(1, numero + 1):
+    for j in range(1, numero+1):
     #print soma
-        j = j + soma
+        
+        print ("j: ", j)
+        soma = soma + j
+        print ("soma: ", soma)
     print("A soma de todos os numeros de 1 a ", numero, " é: ", soma)
     
-except Exception as e: 
-    print("Erro inesperado:", e) 
-    sys.exit(1)
 except ValueError: 
     print("Erro: O valor deve ser um número inteiro.") 
+except ValueError: 
+    print("Erro: Digite um número inteiro válido.") 
