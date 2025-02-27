90753c621dc675ae330a1cc232ea2ea6e9487313
VB-Python <barbosa.vitor@gmail.com>
2025-02-27 20:23:02 +0000

    teste

--- FICHA_INTERMEDIA/ex01.py
+++ FICHA_INTERMEDIA/ex01.py
@@ -1,6 +1,7 @@
 
 ‘’’Escreva um programa que pede ao utilizador um número inteiro e trata 
 erros de entrada.’’’ 
+
 try: 
     numero = int(input("Digite um número inteiro: ")) 
     print("Número inserido:", numero) 
