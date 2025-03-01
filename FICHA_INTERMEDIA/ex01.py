a1b1da554e9a8e517077e2bff03cf4638cd7c531
VB-Python <barbosa.vitor@gmail.com>
2025-03-01 16:35:37 +0000

    uuu

--- FICHA_INTERMEDIA/ex01.py
+++ FICHA_INTERMEDIA/ex01.py
@@ -2,9 +2,10 @@
 '''Escreva um programa que pede ao utilizador um número inteiro e trata 
 erros de entrada.'''
 
-try: 
-    numero = int(input("Digite um número inteiro: ")) 
-    print("Número inserido:", numero) 
-except ValueError: 
+#try: 
+#    numero = int(input("Digite um número inteiro: ")) 
+    numero = int(input("Digite um número superior a 1: "))
+    print ("Número inserido:", numero,) 
+#except ValueError: 
     print("Erro: O valor deve ser um número inteiro.") 
     
