6b8e7963a3ef98f9e8cb6b75bc241f360ddb2c72
VB-Python <barbosa.vitor@gmail.com>
2025-03-01 16:44:02 +0000

    tft

--- FICHA_INTERMEDIA/ex01.py
+++ FICHA_INTERMEDIA/ex01.py
@@ -2,10 +2,8 @@
 '''Escreva um programa que pede ao utilizador um número inteiro e trata 
 erros de entrada.'''
 
-#try: 
-#    numero = int(input("Digite um número inteiro: ")) 
-    numero = int(input("Digite um número superior a 1: "))
-    print ("Número inserido:", numero,) 
-#except ValueError: 
-    print("Erro: O valor deve ser um número inteiro.") 
-    
+try: 
+    numero = int(input("Digite um número: ")) 
+    print("O dobro do número é:", numero * 2) 
+except ValueError: 
+    print("Erro: Digite apenas números inteiros.") 
