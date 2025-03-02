ca4ba088572281c7eabfc55942845201e0c010a9
VB-Python <barbosa.vitor@gmail.com>
2025-03-02 11:02:37 +0000

    xbx

--- FICHA_INTERMEDIA/PART_02/PART02_ex01.py
+++ FICHA_INTERMEDIA/PART_02/PART02_ex01.py
@@ -1,11 +1,13 @@
 '''Reproduz o seguinte Código que tem como objetivo:  
 Criar um programa que leia um ficheiro de texto e exibir o seu conteúdo. 
- 
+
 with open("exemplo.txt", "r") as ficheiro: 
     conteudo = ficheiro.read() 
     print(conteudo) '''
 
-with open("exemplo.txt", "r") as ficheiro: 
+
+#def ler_ficheiro(exemplo.txt)
+with open(exemplo, "r") as ficheiro: 
     conteudo = ficheiro.read() 
     print(conteudo)
 
