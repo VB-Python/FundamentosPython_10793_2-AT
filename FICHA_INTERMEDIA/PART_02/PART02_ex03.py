5f06ed6fa0a25289385aeb28e3325c248f2d8fbc
VB-Python <barbosa.vitor@gmail.com>
2025-03-02 11:36:22 +0000

    fghgf

---
+++ FICHA_INTERMEDIA/PART_02/PART02_ex03.py
@@ -0,0 +1,14 @@
+'''Reproduz o seguinte Código que tem como objetivo:  
+Criar um programa que escreva três linhas num ficheiro novo. 
+ 
+with open("novo_ficheiro.txt", "w") as ficheiro: 
+    ficheiro.write("Linha 1") 
+    ficheiro.write("Linha 2") 
+    ficheiro.write("Linha 3") '''
+
+
+with open("novo_ficheiro.txt", "w") as ficheiro: 
+    ficheiro.write("Linha 1") 
+    ficheiro.write("Linha 2") 
+    ficheiro.write("Linha 3")
+
