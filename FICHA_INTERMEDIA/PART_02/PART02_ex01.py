87c47cacf6794edd84ff4d651e0a959a15db374b
VB-Python <barbosa.vitor@gmail.com>
2025-03-02 10:56:23 +0000

    VNBCG

---
+++ FICHA_INTERMEDIA/PART_02/PART02_ex01.py
@@ -0,0 +1,11 @@
+'''Reproduz o seguinte Código que tem como objetivo:  
+Criar um programa que leia um ficheiro de texto e exibir o seu conteúdo. 
+ 
+with open("exemplo.txt", "r") as ficheiro: 
+    conteudo = ficheiro.read() 
+    print(conteudo) '''
+
+with open("exemplo.txt", "r") as ficheiro: 
+    conteudo = ficheiro.read() 
+    print(conteudo)
+
