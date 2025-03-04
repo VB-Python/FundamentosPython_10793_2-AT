811a27dba806c1f3617c41bcd8dbbce1fb3873a4
VB-Python <barbosa.vitor@gmail.com>
2025-03-04 23:26:47 +0000

    tbfg

--- FICHA_INTERMEDIA/PART_02/01.py
+++ FICHA_INTERMEDIA/PART_02/01.py
@@ -2,9 +2,15 @@
 Criar um programa que escreva três linhas num ficheiro novo. '''
 
 
+import csv 
+try:
+    ficheiro_csv = input("Digite o nome do ficheiro binário a copiar: ")
+    with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
+        leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
+        for linha in leitor: 
+            print(linha["Nome"], "->", linha["Idade"], "anos ->", linha["Cidade"]) 
 
-
-with open("ficheiro.csv", newline='', encoding="utf-8") as ficheiro: 
-    leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
-    for linha in leitor: 
-        print(linha["Nome"], "-", linha["Idade"]) 
+except FileNotFoundError: 
+    print("Erro: O ficheiro não existe.") 
+except Exception as e: 
+    print(f"Ocorreu um erro inesperado: {e}") 
--- FICHA_INTERMEDIA/PART_02/PART02_ex06.py
+++ FICHA_INTERMEDIA/PART_02/PART02_ex06.py
@@ -1,19 +1,18 @@
 '''Cria ou faz download de um ficheiro CSV. De seguida cria um programa 
 que leia o ficheiro CSV e mostre cada linha do mesmo.  '''
 
+
 import csv
 
-ficheiro_csv = input("Digite o nome do ficheiro.csv: ")
-with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
-    leitor = csv.reader(ficheiro) 
-    for linha in leitor: 
-        print(linha)
-    print("\noutra forma: ")
-
-#with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
-   # leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
-    #for linha in leitor: 
-      #  print(linha["Nome"], "-", linha["Idade"]) 
-
-
+try:
+    
+    ficheiro_csv = input("Digite o nome do ficheiro binário a copiar: ")
+    with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
+        leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
+        for linha in leitor: 
+            print(linha["Nome"], "->", linha["Idade"], "anos ->", linha["Cidade"]) 
+except FileNotFoundError: 
+    print("Erro: O ficheiro não existe.") 
+except Exception as e: 
+    print(f"Ocorreu um erro inesperado: {e}") 
 
