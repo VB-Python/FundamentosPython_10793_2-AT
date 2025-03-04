6bc61bfef6aed1ba83656b09ba2228f628bba142
VB-Python <barbosa.vitor@gmail.com>
2025-03-04 22:44:32 +0000

    zxccd

--- FICHA_INTERMEDIA/PART_02/PART02_ex06.py
+++ FICHA_INTERMEDIA/PART_02/PART02_ex06.py
@@ -1,3 +1,9 @@
 '''Cria ou faz download de um ficheiro CSV. De seguida cria um programa 
 que leia o ficheiro CSV e mostre cada linha do mesmo.  '''
 
+import csv
+
+with open("ficheiro.csv", newline='', encoding="utf-8") as ficheiro: 
+    leitor = csv.reader(ficheiro) 
+    for linha in leitor: 
+        print(linha)# ficheiro CSV
