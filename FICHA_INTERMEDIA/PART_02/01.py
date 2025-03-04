4ec483c96e35281f22211889b63b94a0ca811e7e
VB-Python <barbosa.vitor@gmail.com>
2025-03-04 23:09:03 +0000

    kjgkjg

--- FICHA_INTERMEDIA/PART_02/01.py
+++ FICHA_INTERMEDIA/PART_02/01.py
@@ -1,17 +1,10 @@
 '''Reproduz o seguinte Código que tem como objetivo:  
-Criar um programa que escreva três linhas num ficheiro novo. 
- 
-with open("novo_ficheiro.txt", "w") as ficheiro: 
-    ficheiro.write("Linha 1") 
-    ficheiro.write("Linha 2") 
-    ficheiro.write("Linha 3") '''
+Criar um programa que escreva três linhas num ficheiro novo. '''
 
-try:
-    with open("novo_ficheiro.txt", "w", encoding="utf-8") as ficheiro: 
-        ficheiro.write("Linha 1")
-        ficheiro.write("\nLinha 2") 
-        ficheiro.write("\nLinha 3")
-except FileNotFoundError: 
-    print("Erro: O ficheiro não existe.") 
-except Exception as e: 
-    print(f"Ocorreu um erro inesperado: {e}") 
+
+
+
+with open("ficheiro.csv", newline='', encoding="utf-8") as ficheiro: 
+    leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
+    for linha in leitor: 
+        print(linha["Nome"], "-", linha["Idade"]) 
--- FICHA_INTERMEDIA/PART_02/PART02_ex06.py
+++ FICHA_INTERMEDIA/PART_02/PART02_ex06.py
@@ -2,17 +2,12 @@
 que leia o ficheiro CSV e mostre cada linha do mesmo.  '''
 
 import csv
-import os
 
-try:
-    # Solicitar entrada do utilizador 
-    ficheiro_csv = input("Digite o nome do ficheiro csv: ")
-    if os.path.exists(ficheiro_csv):
-        with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
-        #with open("ficheiro.csv", "r", encoding="utf-8") as ficheiro:
-            leitor = csv.reader(ficheiro) 
-            for linha in leitor: 
-                print(linha) # ficheiro CSV
+ficheiro_csv = input("Digite o nome do ficheiro.csv: ")
+with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
+    leitor = csv.reader(ficheiro) 
+    for linha in leitor: 
+        print(linha)
     print("\noutra forma: ")
 
 #with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
