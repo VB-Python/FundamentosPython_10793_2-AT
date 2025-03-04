838611bc9a62bc37ecec1fc9583d30da4aca8a55
VB-Python <barbosa.vitor@gmail.com>
2025-03-04 23:00:40 +0000

    nk,kj,k

--- FICHA_INTERMEDIA/PART_02/PART02_ex06.py
+++ FICHA_INTERMEDIA/PART_02/PART02_ex06.py
@@ -2,18 +2,23 @@
 que leia o ficheiro CSV e mostre cada linha do mesmo.  '''
 
 import csv
+import os
 
-#with open("ficheiro.csv", newline='', encoding="utf-8") as ficheiro: 
-#with open("ficheiro.csv", "r", encoding="utf-8") as ficheiro:
-    leitor = csv.reader(ficheiro) 
-    for linha in leitor: 
-        print(linha) # ficheiro CSV
+try:
+    # Solicitar entrada do utilizador 
+    ficheiro_csv = input("Digite o nome do ficheiro csv: ")
+    if os.path.exists(ficheiro_csv):
+        with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
+        #with open("ficheiro.csv", "r", encoding="utf-8") as ficheiro:
+            leitor = csv.reader(ficheiro) 
+            for linha in leitor: 
+                print(linha) # ficheiro CSV
+    print("\noutra forma: ")
 
-
-with open("dados.csv", newline='', encoding="utf-8") as ficheiro: 
-    leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
-    for linha in leitor: 
-        print(linha["Nome"], "-", linha["Idade"]) 
+#with open(ficheiro_csv, newline='', encoding="utf-8") as ficheiro: 
+   # leitor = csv.DictReader(ficheiro)  # Converte cada linha num dicionário 
+    #for linha in leitor: 
+      #  print(linha["Nome"], "-", linha["Idade"]) 
 
 
 
