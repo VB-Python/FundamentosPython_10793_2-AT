5e06f481b9ca11595e79054e5597ff7d53b0c20d
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 00:19:45 +0000

    ngg

--- FICHA_INTERMEDIA/PART03/banco_dados.py
+++ FICHA_INTERMEDIA/PART03/banco_dados.py
@@ -7,14 +7,7 @@ conn = sqlite3.connect('empresa.db')
 cursor = conn.cursor()
 
 #Criar tabela de funcionários
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS funcionarios ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        cargo TEXT NOT NULL,
-        salario REAL NOT NULL
-    )
-''')
+cursor.execute("CREATE TABLE IF NOT EXISTS funcionarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, cargo TEXT NOT NULL, salario REAL NOT NULL)")
 #Guardar as mudanças e fechar a conexão
 conn.commit()
 conn.close()
