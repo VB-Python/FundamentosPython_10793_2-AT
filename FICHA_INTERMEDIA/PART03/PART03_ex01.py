79f74330b4dc6b9dda17e736bfb764a13d60891f
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 00:38:29 +0000

    kkkk

--- FICHA_INTERMEDIA/PART03/PART03_ex01.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex01.py
@@ -5,13 +5,20 @@ Verifiquei o criar de um ficheiro chamado "empresa.db".'''
 import banco_dados
 import sqlite3
 
-#banco_dados
+banco_dados
 
-conexao = sqlite3.connect("empresa.db")
+try:
+    conexao = sqlite3.connect("empresa.db")
 
 
-cursor = conexao.cursor()
+    cursor = conexao.cursor()
 
-print("Base de dados criada e conectada com sucesso!")
+    print("Base de dados criada e conectada com sucesso!")
 
-conexao.close()
+    conexao.close()
+
+except FileNotFoundError: 
+    print("Erro: O ficheiro não existe.") 
+except Exception as e: 
+    print(f"Ocorreu um erro inesperado: {e}") 
+
