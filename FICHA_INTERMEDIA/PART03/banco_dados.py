ad289f71a6325eebe5fc8b9ab4ebf5cb51f93484
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 00:28:53 +0000

    c c

--- FICHA_INTERMEDIA/PART03/banco_dados.py
+++ FICHA_INTERMEDIA/PART03/banco_dados.py
@@ -8,8 +8,6 @@ cursor = conn.cursor()
 
 #Criar tabela de funcionários
 
-#cursor.execute("CREATE TABLE IF NOT EXISTS funcionarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, cargo TEXT NOT NULL, salario REAL NOT NULL)")
-
 cursor.execute('''
    CREATE TABLE IF NOT EXISTS funcionarios (
        id INTEGER PRIMARY KEY AUTOINCREMENT,
