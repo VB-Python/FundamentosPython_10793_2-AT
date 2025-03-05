6eac4f3e3a61787fba8ea08abd0a9c9fb42a2ae2
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 02:05:39 +0000

    dd

--- FICHA_INTERMEDIA/PART03/PART03_ex02_2.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex02_2.py
@@ -1,6 +1,6 @@
 '''Dados inseridos em PART03_PONTO_2.py'''
 
-'''Acrescenta mais 2 funcionários à basa de dados, alterando o código acima, PART03_PONTO_2.py'''
+'''Acrescenta mais 2 funcionários à basa de dados, alterando o código acima, PART03_ex02_1.py'''
 
 
 
