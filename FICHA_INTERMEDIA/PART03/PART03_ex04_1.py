1b9eb20c6540b885a8aa7846d040303731d00c86
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 02:08:09 +0000

    ccc

--- FICHA_INTERMEDIA/PART03/PART03_ex04_1.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex04_1.py
@@ -10,5 +10,16 @@ cursor = conn.cursor()
 #Atualizar o salário de Pedro Santos
 cursor.execute("UPDATE funcionarios SET salario = 3000.00 WHERE nome = 'Pedro Santos'")
 
+#Consulta todos os funcionarios
+cursor.execute("SELECT * FROM funcionarios")
+#retorna todos os registos
+funcionarios = cursor.fetchall()
+
+#Exibir os resultados
+for funcionario in funcionarios:
+    #exibe todos os funcionarios
+    print(funcionario)
+
+
 conn.commit()
 conn.close()
