ad23692fb73b2a4d117d34c36d5be98321417bc0
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 00:35:02 +0000

    dd

--- FICHA_INTERMEDIA/PART03/PART03_ex01.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex01.py
@@ -2,3 +2,16 @@
 Criei, e executei o ficheiro "banco_dados.py".
 Verifiquei o criar de um ficheiro chamado "empresa.db".'''
 
+import banco_dados
+import sqlite3
+
+#banco_dados
+
+conexao = sqlite3.connect("empresa.db")
+
+
+cursor = conexao.cursor()
+
+print("Base de dados criada e conectada com sucesso!")
+
+conexao.close()
