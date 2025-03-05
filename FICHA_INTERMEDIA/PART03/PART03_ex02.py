00c09499db53ba6a1ea2029fc99207475ee2ccaf
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 01:03:19 +0000

    fff

--- FICHA_INTERMEDIA/PART03/PART03_ex02.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex02.py
@@ -13,19 +13,29 @@ cursor = conn.cursor()
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Mario', 'Presidente', 6500)")
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Soares', 'Ministro', 7500)")
 
+#Guardar mudanças
+conn.commit()
+#Fechar conexão
+conn.close()
 
+
+
+import sqlite3
+
+conn = sqlite3.connect('empresa.db')
+cursor = conn.cursor()
 
 #Verificar nomes criados
 cursor.execute("SELECT * FROM funcionarios")
 nomes = cursor.fetchall() #retorna todos os registos
 
 #Exibe os funcionarios
-    for nome in nomes:
+#    for nome in nomes:
         #if funcionario == "Mario" and funcionario == "Soares"
            # print("Funcionários acrescentados com Sucesso")
         #else
            # print("Acrescentar 2 funcionários sem sucesso")
-        print(nome)
+#       print(nome)
 
 #Guardar mudanças
 conn.commit()
