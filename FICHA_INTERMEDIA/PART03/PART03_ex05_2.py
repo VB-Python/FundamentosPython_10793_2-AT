150427b51e547014c0307fda348dc5095bf471f8
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 02:24:02 +0000

    bb

--- FICHA_INTERMEDIA/PART03/PART03_ex05_2.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex05_2.py
@@ -6,8 +6,8 @@ import sqlite3
 conn = sqlite3.connect('empresa.db')
 cursor = conn.cursor()
 
-#Eliminar um funcionário
-cursor.execute("DELETE FROM funcionarios WHERE nome = 'Mariana Costa'")
+#Elimina todos os salários inferiores a 3000.00
+cursor.execute("DELETE FROM funcionarios WHERE salario < 3000.00")
 
 #Consulta todos os funcionarios
 cursor.execute("SELECT * FROM funcionarios")
