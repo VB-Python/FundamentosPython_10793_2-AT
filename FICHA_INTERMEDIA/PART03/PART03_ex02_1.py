0fb616538bcd0b9bbc821dd096a870906fd7b192
VB-Python <barbosa.vitor@gmail.com>
2025-03-05 02:04:16 +0000

    ggg

--- FICHA_INTERMEDIA/PART03/PART03_ex02_1.py
+++ FICHA_INTERMEDIA/PART03/PART03_ex02_1.py
@@ -10,6 +10,17 @@ cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Ana Sil
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Pedro Santos', 'Programador', 4500)")
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Mariana Costa', 'Designer', 5500)")
 
+
+#Consulta todos os funcionarios
+cursor.execute("SELECT * FROM funcionarios")
+#retorna todos os registos
+funcionarios = cursor.fetchall()
+#Exibir os resultados
+for funcionario in funcionarios:
+#exibe todos os funcionarios
+    print(funcionario)
+
+
 #Guardar mudanças e fechar conexão
 conn.commit()
 conn.close()
