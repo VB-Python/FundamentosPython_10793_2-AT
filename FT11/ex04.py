a566d6f2ed9dc82cc759cb80027b1bde45fd9374
VB-Python <barbosa.vitor@gmail.com>
2025-03-06 21:35:29 +0000

    jhku

--- FT11/ex04.py
+++ FT11/ex04.py
@@ -1,7 +1,7 @@
 '''Escreve uma função em Python que dada uma palavra retorne o número de
  vogais nessa palavra. '''
  
- def conta_vogais(palavra):
+def conta_vogais(palavra):
     vogais = 'aeiou'
     count = 0
     for letra in palavra.lower():
