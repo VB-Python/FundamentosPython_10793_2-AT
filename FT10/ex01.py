bd3a067399160e4b159686fb85bcb1db78f042e1
VB-Python <barbosa.vitor@gmail.com>
2025-03-06 21:21:33 +0000

    tt

---
+++ FT10/ex01.py
@@ -0,0 +1,32 @@
+'''Reproduz o seguinte programa.'''
+
+txt =" uFcd proGRAMação eM pyTHON "
+
+#Imprimir texto
+
+print(txt)
+
+#Imprimir Texto sem espaçamento inicial
+txt=txt.strip()
+print(txt)
+
+#Imprimir frase até à palavra na 13ª posição
+
+print(txt[:13])
+
+#Imprimir últimos 5 caracteres da frase
+print(txt[-5:])
+
+#Imprimir frase em maiúsculas
+
+txt=txt.upper()
+print(txt)
+
+#Formatação de strings
+
+nome="Sandra Oliveira"
+print("O {} gosta muito da {}".format(nome, txt))
+
+#alinea g)
+
+print(f'O {nome} gosta muito da {txt}')
---
+++ FT10/ex04.py
@@ -0,0 +1,12 @@
+'''. Considere a seguinte variável:
+Txt="""Python é uma linguagem de programação
+de alto nível, interpretada de script, imperativa, orientada a objetos,
+funcional, de tipagem dinâmica e forte."""
+Escreve um programa em Python que
+a. Imprima o texto anterior todo em maiúsculas;
+b. Peça uma palavra ao utilizador e verifique se a mesma está ou não no
+texto, devolvendo o resultado ao utilizador.
+c. Imprima o número de vezes que a letra ‘O’ ocorre no texto
+d. Substitua todaa as ocorrências da letra ‘P’ no texto por ‘_’'''
+
+
