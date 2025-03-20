178686c1966e4618f710e4fbaaef2fc11eabffda
VB-Python <barbosa.vitor@gmail.com>
2025-03-20 23:52:34 +0000

    g

---
+++ FICHA_FINAL/FAVEnunciadoGeral/ex04_FAVEGeral.py
@@ -0,0 +1,8 @@
+'''4. Utilizando a biblioteca NumPy : 
+a. Cria um array de números 1 com shape (8, 2) 
+b. Cria um array de zeros com shape (5, 7) 
+c. Subtraia o novo array de outro com dados aleatórios e armazene o numa variável 
+chamada subarray 
+d. Calcula a média dos valores do array subarray '''
+
+
