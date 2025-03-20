c1f1d584fc986dcc8bbeb7c6d5ace477c73bfa22
VB-Python <barbosa.vitor@gmail.com>
2025-03-20 23:17:27 +0000

    bv

--- FICHA_FINAL/FAVEnunciadoGeral/ex03_FAVEGeral.py
+++ FICHA_FINAL/FAVEnunciadoGeral/ex03_FAVEGeral.py
@@ -2,15 +2,22 @@
 valores dessa lista, o número de elementos da lista e a media desses valores. 
 Implementa tratamento de exceções no teu código (try…except…else..finally). '''
 
-
-
-
 import funcao
-print("*** Programa para cálculo:\n Da soma, Do número, e Da média\n dos numeros introduzidos ***")
-dados = input("Escreve uma quantidade de números, separados por espaço: ")
-soma, conta_numeros, media = funcao.numeros(dados)
-print(f"A soma de todos os números é {soma}; a lista tem {conta_numeros} números e a média é {media}")
-print(f"Sum: ", soma)
-print(f"Number of items: ", conta_numeros)
-print(f"Average: ", media)
 
+try:
+
+    print("*** Programa para cálculo:\n Da soma, Do número, e Da média\n dos numeros introduzidos ***")
+    
+    dados = input("Escreve uma quantidade de números, separados por espaço: ")
+    soma, conta_numeros, media = funcao.numeros(dados)
+    
+    print(f"A soma de todos os números é {soma}; a lista tem {conta_numeros} números e a média é {media}\n")
+    print(f"Sum: ", soma)
+    print(f"Number of items: ", conta_numeros)
+    print(f"Average: ", media)
+
+except ValueError:
+    print("Erro: Por favor, insira apenas números válidos.")
+
+finally:
+    print("\nFim do processo.\n")
