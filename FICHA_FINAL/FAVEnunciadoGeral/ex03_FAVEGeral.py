3adf3768432c7cfeae389bcbcdc13a9708009d26
VB-Python <barbosa.vitor@gmail.com>
2025-03-20 23:32:09 +0000

    vcbc

--- FICHA_FINAL/FAVEnunciadoGeral/ex03_FAVEGeral.py
+++ FICHA_FINAL/FAVEnunciadoGeral/ex03_FAVEGeral.py
@@ -9,15 +9,22 @@ try:
     print("*** Programa para cálculo:\n Da soma, Do número, e Da média\n dos numeros introduzidos ***")
     
     dados = input("Escreve uma quantidade de números, separados por espaço: ")
-    soma, conta_numeros, media = funcao.numeros(dados)
-    
-    print(f"A soma de todos os números é {soma}; a lista tem {conta_numeros} números e a média é {media}\n")
+    soma, conta_numeros, media = funcao.numeros(dados) #chama em funcao.py
+
+# Se detectar algo que não seja um número (como texto ou caractere especial), 
+# o programa capturará esse erro e imprimirá uma mensagem de erro.
+except ValueError:
+    print("Erro: Por favor, insira apenas números válidos.")
+
+# Se não ocorrer nenhum erro, ele executará o cálculo e 
+# exibirá o número de entradas, a soma e a média dos números fornecidos.
+else:
+    print(f"\nA soma de todos os números é {soma}; a lista tem {conta_numeros} números e a média é {media}\n")
     print(f"Sum: ", soma)
     print(f"Number of items: ", conta_numeros)
     print(f"Average: ", media)
 
-except ValueError:
-    print("Erro: Por favor, insira apenas números válidos.")
-
+# Independentemente de ocorrer um erro ou não, 
+# o bloco finally será sempre executado, mostrando "Fim do processo."
 finally:
     print("\nFim do processo.\n")
