66c18bb15bec9ba8e85f2ba33b343c0ffa5f5f13
VB-Python <barbosa.vitor@gmail.com>
2025-03-20 21:36:15 +0000

    fdfb

---
+++ FICHA_FINAL/FAVEnunciadoGeral/ex01_FAVEGeral.py
@@ -0,0 +1,57 @@
+'''1. Leia uma cadeia de caracteres no formato “DD/MM/AAAA” e copie o dia, mês e ano para 
+três variáveis inteiras.'''
+
+try:
+    # Leitura da data no formato DD/MM/AAAA
+    data = input("Digite a data (DD/MM/AAAA): ")
+
+    # Separando o dia, mês e ano
+    dia, mes, ano = map(int, data.split('/'))
+    
+    if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+        if dia < 32:
+             # Exibindo os valores
+            print(f"Dia: {dia}")
+            print(f"Mês: {mes}")
+            print(f"Ano: {ano}")
+        else:
+            print(f"O mês {mes} não tem {dia} dias")
+
+    elseif:
+        if mes == 4 or mes == 6 or mes == 9 or mes == 11:
+            if dia < 31:
+                # Exibindo os valores
+                print(f"Dia: {dia}")
+                print(f"Mês: {mes}")
+                print(f"Ano: {ano}")
+            else:
+                print(f"O mês {mes} não tem {dia} dias")
+                
+    elseif:
+        if mes == 2:
+            if ano % 400 == 0 or (ano % 100 != 0 and ano % 4 == 0):
+                #print("O ano" , ano, "é Bissexto")
+                if dia < 30:
+                    # Exibindo os valores
+                    print(f"Dia: {dia}")
+                    print(f"Mês: {mes}")
+                    print(f"Ano: {ano}")
+                else:
+                    print(f"Em {ano} o mês {mes} não tem {dia} dias")
+            else:
+                #print("O ano" , ano, "não é Bissexto")
+                if dia < 29:
+                    # Exibindo os valores
+                    print(f"Dia: {dia}")
+                    print(f"Mês: {mes}")
+                    print(f"Ano: {ano}")
+                else:
+                    print(f"Em {ano} o mês {mes} não tem {dia} dias")
+        
+    # Exibindo os valores
+    #print(f"Dia: {dia}")
+    #print(f"Mês: {mes}")
+    #print(f"Ano: {ano}")
+
+except ValueError:
+    print("Erro: O formato da data deve ser DD/MM/AAAA e números inteiros.")
