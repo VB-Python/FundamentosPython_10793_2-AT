4e63a28d5573e52af1a66e27b13f9dcc7d342bb2
VB-Python <barbosa.vitor@gmail.com>
2025-03-20 22:08:29 +0000

    cv c

--- FICHA_FINAL/FAVEnunciadoGeral/ex01_FAVEGeral.py
+++ FICHA_FINAL/FAVEnunciadoGeral/ex01_FAVEGeral.py
@@ -1,6 +1,10 @@
 '''1. Leia uma cadeia de caracteres no formato “DD/MM/AAAA” e copie o dia, mês e ano para 
 três variáveis inteiras.'''
 
+Dia = int
+Mes = int
+Ano = int
+
 try:
     # Leitura da data no formato DD/MM/AAAA
     data = input("Digite a data (DD/MM/AAAA): ")
@@ -11,28 +15,35 @@ try:
     if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
         if dia < 32:
              # Exibindo os valores
+            Dia = dia
+            Mes = mes
+            Ano = ano
             print(f"Dia: {dia}")
             print(f"Mês: {mes}")
             print(f"Ano: {ano}")
         else:
             print(f"O mês {mes} não tem {dia} dias")
 
-    elseif:
-        if mes == 4 or mes == 6 or mes == 9 or mes == 11:
+    elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
             if dia < 31:
                 # Exibindo os valores
+                Dia = dia
+                Mes = mes
+                Ano = ano
                 print(f"Dia: {dia}")
                 print(f"Mês: {mes}")
                 print(f"Ano: {ano}")
             else:
                 print(f"O mês {mes} não tem {dia} dias")
                 
-    elseif:
-        if mes == 2:
+    elif mes == 2:
             if ano % 400 == 0 or (ano % 100 != 0 and ano % 4 == 0):
                 #print("O ano" , ano, "é Bissexto")
                 if dia < 30:
                     # Exibindo os valores
+                    Dia = dia
+                    Mes = mes
+                    Ano = ano
                     print(f"Dia: {dia}")
                     print(f"Mês: {mes}")
                     print(f"Ano: {ano}")
@@ -42,6 +53,9 @@ try:
                 #print("O ano" , ano, "não é Bissexto")
                 if dia < 29:
                     # Exibindo os valores
+                    Dia = dia
+                    Mes = mes
+                    Ano = ano
                     print(f"Dia: {dia}")
                     print(f"Mês: {mes}")
                     print(f"Ano: {ano}")
